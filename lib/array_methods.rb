e0d20c9718b71ef6521b91027262825ffec06f2b
Nick Solonyy <nicksolony@gmail.com>
2019-10-24 03:38:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,13 +2,11 @@ def find_element_index(array, value_to_find)
   array.size.times do |index|
    if array[index] == value_to_find
     puts index
-  #  else
-   #   return nil
     end
   end
 end
 scale = [2,3,4,5]
-find_element_index(scale,5)
+find_element_index(scale,2)
 
 
 def find_max_value(array)
