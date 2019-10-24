40c2dce0dacfbfa9ff8975d167e41f9a37c823fe
Nick Solonyy <nicksolony@gmail.com>
2019-10-24 03:40:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   array.size.times do |index|
   # if array[index] == value_to_find
-    puts index if array[index] == value_to_find
+    return index if array[index] == value_to_find
   end
 end
 scale = [2,3,4,5]
