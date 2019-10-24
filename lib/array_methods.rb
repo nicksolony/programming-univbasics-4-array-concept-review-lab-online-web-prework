d36873fa7c5932fbc24c85787c05c0cc6d2c3f0c
Nick Solonyy <nicksolony@gmail.com>
2019-10-24 03:26:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,8 +2,8 @@ def find_element_index(array, value_to_find)
   array.size.times do |index|
     if array[index] == value_to_find
       return index
-    else
-      return nil
+  #  else
+   #   return nil
     end
   end
 end
