c171a45bc1be4403550838e4cd4fce9dd248d949
Nick Solonyy <nicksolony@gmail.com>
2019-10-24 03:28:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,8 +7,8 @@ def find_element_index(array, value_to_find)
     end
   end
 end
-scale = [2,3,4,5]
-find_element_index(scale,9)
+#scale = [2,3,4,5]
+#find_element_index(scale,9)
 
 
 def find_max_value(array)
